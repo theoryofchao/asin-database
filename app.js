e7db1028edcba8325c6245d33f6ca1dcf57684ce
Roger Chao <roger.ti.chao@gmail.com>
2018-05-28 15:35:04 -0700

    got post route working to get asin

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(bodyParser.jason());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
