58e169265a25dd6157cbf831e1fa7fe7075528a1
Roger Chao <roger.ti.chao@gmail.com>
2018-05-30 01:46:29 -0700

    created functions for finding the correct parameters for product dimensions as well as stripping out useless data in array

--- routes/api.js
+++ routes/api.js
@@ -7,10 +7,12 @@ const cheerio = require('cheerio');
 router.post('/asin', function(req, res, next) {
   let asin_url = req.body.asin_url;
 
+  //check if already loaded into database?
+
   const token = 'HQfTDQ5Cun8TSUrsxgLevg';
 
   let options = {
-    method: 'POST',
+    method: 'GET',
     uri: `https://api.proxycrawl.com/?token=${token}&url=${encodeURIComponent(asin_url)}`,
     transform: (body) => {
       return cheerio.load(body);  //When promise resolves, transforms function by loading into cheerio (for parsing)
@@ -19,11 +21,21 @@ router.post('/asin', function(req, res, next) {
 
   rp(options)
   .then(($) => {
-    let detailBullet = $('#detail-bullets .bucket .content ul').text();
-    let detailBullet2 = detailBullet.split(/[\r\n]+/);
 
-    console.log(detailBullet2);
-    let salesRank = $('#SalesRank').html();
+    let categories = $('#wayfinding-breadcrumbs_feature_div ul').text();
+    categories = cleanArray(categories.split(/[\r\n]+/));
+
+    console.log(categories);
+
+    let detailBullet = $('#detail-bullets .bucket .content ul').text();
+    detailBullet = cleanArray(detailBullet.split(/[\r\n]+/));
+    // console.log(detailBullet);
+
+    let productDimensions = findParameters(detailBullet, "product dimensions");
+
+    // console.log(productDimensions);
+
+    let salesRank = $('#SalesRank').html(); //TODO: take salesRank and separate into rank and category?
 
     return res.send(detailBullet);
   })
@@ -34,5 +46,28 @@ router.post('/asin', function(req, res, next) {
   // return res.send(req.body);
   // res.render('index', { title: 'Express' });
 });
+
+function cleanArray(actual) {
+  var newArray = [];
+  actual.forEach((element) => {
+    if(element.trim()) {
+      newArray.push(element.trim());
+    }
+  });
+  return newArray;
+}
+
+let findParameters = (array, keyword) => {
+  for(let i = 0; i < array.length; i++) {
+    if(array[i].match(new RegExp(keyword, "i"))) {
+      let description = array[i].split(":")[1].trim();
+      if(description) {
+        return description;
+      } else {
+        return array[i+1].trim();
+      }
+    }
+  }
+};
 
 module.exports = router;
