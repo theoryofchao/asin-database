2e36017ee6f87bbf4f39d94e7737bdbb4d5801d0
Roger Chao <roger.ti.chao@gmail.com>
2018-05-30 22:23:50 -0700

    created text input and submit

--- asin-frontend/src/App.js
+++ asin-frontend/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from './SearchBar.js';
 
 class App extends Component {
+
+  // this.state = {
+  //   url: 'https://www.amazon.com/dp/B004TOD9HO'
+  // };
+
   render() {
     return (
       <div className="App">
@@ -10,6 +16,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Jungle Scout ASIN Crawler</h1>
         </header>
+        <SearchBar name="test"/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ asin-frontend/src/SearchBar.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+
+  // onUserNameChange = e => {
+  //   if (e.key === 'Enter') {
+  //     this.props.changeUserName(e.target.value);
+  //   }
+  // }
+
+  // onMessageEnter = e => {
+  //   if (e.key === 'Enter') {
+  //     this.props.newMessage(e.target.value);
+  //     e.target.value = '';
+  //   }
+  // }
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+  handleChange(event) {
+      this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert(this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.value} onChange={this.handleChange}/>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+
+}
+
+export default SearchBar;
+
